29228542c5571d84ebc2529870ad872f3d7cdfee
Anna Wiesiołek <ania9052@wp.pl>
2020-05-21 22:08:44 +0200

    js changes

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = "Anna";
+const age = 29;
+
+console.log(`Witaj, nazywam się ${firstName} i mam ${age} lat`);
+
+const heading = document.querySelector(".header-title--js");
+
+heading.innerHTML = "Moja przygoda z programowaniem czyli <br> Co ten frontend";
