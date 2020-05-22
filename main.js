7c6c96a3e5221f650893094d9c01019a630406b3
Anna Wiesiołek <ania9052@wp.pl>
2020-05-22 15:11:00 +0200

    BEM changes again

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const age = 29;
 
 console.log(`Witaj, nazywam się ${firstName} i mam ${age} lat`);
 
-const heading = document.querySelector(".header-title--js");
+const heading = document.querySelector(".header__title--js");
 
 heading.innerHTML = "Moja przygoda z programowaniem czyli <br> Co ten frontend";
